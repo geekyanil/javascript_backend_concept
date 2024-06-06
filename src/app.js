22a40239a0cf921c801c3423f97062f0f24e2e9d
geekyanil <ay439078@gmail.com>
2024-06-06 11:47:49 +0545

    javscript_backend

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+const app = express();
+
+// app.use(cors())
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// IMPORT ROUTES
+import userRoutes from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRoutes);
+
+export { app };
